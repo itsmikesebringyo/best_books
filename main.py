9c784c9dc47585d5d7ed70331c6278dee40cff4c
itsmikesebringyo <michaelsebring@mikeys-MacBook.local>
2022-01-30 10:57:07 -0600

    added pseudocode providing outline of plan to get data into storage

--- main.py
+++ main.py
@@ -19,6 +19,23 @@ def main():
     t_token = os.getenv('TWITTER_TOKEN')
     g_key = os.getenv('GOOGLE_BOOKS_KEY')
 
+    # TODO - hit twitter endpoint to get list of tweets of interest for a day
+        # paginate through tweet responses using 'next_token'
+    
+    # TODO - for each tweet in our list, 
+        # get sentiment score on raw text
+        # remove mentions, hashtags, links from tweet
+        # use NER to get entities
+        # for each entity
+            # remove stopwords so only keywords remain?
+            # check google and amazon api's to check if entity is a book
+        # if book, perform some sort of matching (fuzzy?) from google/amazon to tweet entity
+        # if book, try grabbing google/amazon rating
+        # final data item should look like:
+            # tweet_id, original_tweet, extracted_book, sentiment_score, google_rating, amazon_rating
+
+    
+
     with open('example_response.json') as f:
         response = f.read()
     sample_response = json.loads(response)
