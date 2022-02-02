4621857eeab762c2ed2f2ae4918d4954c4da8ed5
itsmikesebringyo <michaelsebring@mikeys-MacBook.local>
2022-02-01 21:15:49 -0600

    updated main and utils to grab author names

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-import json
-from utils import load_env, get_sentiment, clean_tweet
+from utils import get_yesterday_timeframe, load_env, parse_tweets
 
 
 CLEANING_MAP = {
@@ -19,38 +18,37 @@ def main():
     t_token = os.getenv('TWITTER_TOKEN')
     g_key = os.getenv('GOOGLE_BOOKS_KEY')
 
-    # TODO - hit twitter endpoint to get list of tweets of interest for a day
-        # paginate through tweet responses using 'next_token'
+    time_start, time_end = get_yesterday_timeframe()
+
+    twitter_request = {
+        'url': 'https://api.twitter.com/2/tweets/search/recent',
+        'headers': {'Authorization': f'Bearer {t_token}'},
+        'params': {'query': 'lang:en (#BookReview OR #BookRecommendations)', 
+                   'max_results': 10,
+                   'tweet.fields': 'created_at',
+                   'start_time': time_start,
+                   'end_time': time_end}
+    }
+
+    tweet_data = parse_tweets(twitter_request)
+    print(tweet_data)
     
-    # TODO - for each tweet in our list, 
-        # get sentiment score on raw text
-        # remove mentions, hashtags, links from tweet
-        # use NER to get entities
-        # for each entity
-            # remove stopwords so only keywords remain?
-            # check google and amazon api's to check if entity is a book
-        # if book, perform some sort of matching (fuzzy?) from google/amazon to tweet entity
-        # if book, try grabbing google/amazon rating
-        # final data item should look like:
-            # tweet_id, original_tweet, extracted_book, sentiment_score, google_rating, amazon_rating
 
-    
-
-    with open('example_response.json') as f:
-        response = f.read()
-    sample_response = json.loads(response)
-    data = sample_response.get('data')
-    m_data = sample_response.get('meta')
+    # with open('example_response.json') as f:
+    #     response = f.read()
+    # sample_response = json.loads(response)
+    # data = sample_response.get('data')
+    # m_data = sample_response.get('meta')
 
 
-    for tweet in data:
-        raw_text = tweet.get('text')
-        print(raw_text)
-        clean_text = clean_tweet(raw_text, CLEANING_MAP.values()) 
-        print(clean_text)
-        raw_score = get_sentiment(raw_text)
-        print(raw_score)
-        print('\n')
+    # for tweet in data:
+    #     raw_text = tweet.get('text')
+    #     print(raw_text)
+    #     clean_text = clean_tweet(raw_text, CLEANING_MAP.values()) 
+    #     print(clean_text)
+    #     raw_score = get_sentiment(raw_text)
+    #     print(raw_score)
+    #     print('\n')
 
 
 
--- utils.py
+++ utils.py
@@ -1,12 +1,21 @@
-import json
 import re
-import os
 import requests
+import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 import spacy
-import nltk
+# import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+NLP = spacy.load('en_core_web_sm')
+
+CLEANING_MAP = {
+    'mentions': '@[a-zA-Z0-9_]+',
+    # 'query_hashtags': '#[Bb]ook[Rr]eview|#[Bb]ook[Rr]ecommendations',
+    'all_hashtags': '#[A-Za-z0-9_]+',
+    'links': 'http\S+'
+}
 
 
 def load_env():
@@ -41,6 +50,82 @@ def connect_to_endpoint(url, headers, params):
     return response.json()
 
 
+def get_yesterday_timeframe():
+    """
+    Returns yesterday's start and end time.
+    """
+    today = datetime.datetime.combine(datetime.date.today(), datetime.time(0,0,0)).astimezone()
+    yest = today - datetime.timedelta(days=1)
+    time_start = yest.isoformat('T')
+    time_end = today.isoformat('T')
+    
+    return time_start, time_end
+
+
+def get_authors(tweet_text):
+    """
+    Takes tweet text as input and returns a list of people mentioned 
+    """
+    doc = NLP(tweet_text.title())
+
+    authors = []
+    for ent in doc.ents:
+        if (ent.label_ == "PERSON") and (len(ent.text.split()) > 1):
+            authors.append(ent.text)
+
+    return authors
+
+
+def parse_tweets(request_object, tweets_list=[]):
+    """
+    Takes twitter request object as input, connects to twitter endpoint, parses the response,
+    and returns a list of lists of tweets of interest
+    """
+    # first, get the response
+    response = connect_to_endpoint(**request_object)
+    data = response.get('data')
+    m_data = response.get('meta')
+
+    for tweet in data:
+        tweet_id = tweet.get('id')
+        text = tweet.get('text')
+        created_at = tweet.get('created_at')
+        
+        # parse through tweet logic here...
+        raw_sentiment = get_sentiment(text)
+
+        clean_text = clean_tweet(text, CLEANING_MAP.values())
+        print(clean_text)
+        # for now, we'll grab author mentions
+        # later, train a custom model to recognize book titles
+        authors = get_authors(clean_text)
+
+        for author in authors:
+            print(author)
+            record = [tweet_id, created_at, text, author]
+            tweets_list.append(record)
+
+        # TODO: once a custom NLP model is created and works for recognizing book titles, implement logic below
+        # use NER to get entities
+        # for each entity
+            # remove stopwords so only keywords remain?
+            # check google and amazon api's to check if entity is a book
+        # if book, perform some sort of matching (fuzzy?) from google/amazon to tweet entity
+        # if book, try grabbing google/amazon rating
+        # final data item should look like:
+            # tweet_id, created_at, original_tweet, extracted_book, sentiment_score, google_rating, amazon_rating
+        
+
+    # next_token = m_data.get('next_token')
+    # # if next_token exists, then recursively parse tweets again
+    # if next_token:
+    #     request_object['params']['pagination_token'] = next_token
+
+    #     parse_tweets(request_object, tweets_list)
+
+    return tweets_list
+
+
 def get_sentiment(tweet_text):
     """
     Takes text of tweet as input and returns a sentiment score
@@ -68,7 +153,7 @@ def clean_tweet(text, reg_expr):
 # key = os.getenv('GOOGLE_BOOKS_KEY')
 # headers = generate_header(token)
 
-# hashtags = ['#BookReview', '#BookRecommendations']
-# hashtags_str = ' OR '.join(hashtags)
+hashtags = ['#BookReview', '#BookRecommendations']
+hashtags_str = ' OR '.join(hashtags)
 # search_url = "https://api.twitter.com/2/tweets/search/recent"
-# query_params = {'query': f'lang:en ({hashtags_str})'}
+query_params = {'query': f'lang:en ({hashtags_str}) tweet.fields=created_at'}
